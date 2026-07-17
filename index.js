2d783127069aa7b14ae5da775ebd05d85aa012d5
Ini-Amin <amin.core@proton.me>
2026-07-17 22:20:41 +0700

    Stage 1: root and health endpoints

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 const app = express();
 const PORT = 3000;
-
+app.use(express.json());
 app.get('/', (req, res) => {
-    res.send('Hello,World!');
+    res.json({
+        "name": "Task API",
+        version: "1.0",
+        endpoints: ["/tasks"]
+    });
 });
-
+app.get('/health', (req, res) => {
+    res.json({status: "ok"});
+})
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
